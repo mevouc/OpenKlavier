8a41279be0c94f1b7faeb7f030c32911950906fa
Meven Courouble <meven.courouble@vouc.me>
2026-04-06 00:05:11 +0200

    ui: Create config & theme files

---
+++ src/Klavier.UI/Options/UIConfig.cs
@@ -0,0 +1,11 @@
+using Klavier.Core.Music;
+
+namespace Klavier.UI.Options;
+
+public class UIConfig
+{
+    public bool Topmost { get; init; }
+    public bool ShowKeyLabels { get; init; } = true;
+    public bool ShowNoteLabels { get; init; } = true;
+    public NoteNameStyle NoteNameStyle { get; init; } = NoteNameStyle.Scientific;
+}
---
+++ src/Klavier.UI/Theme/KlavierTheme.cs
@@ -0,0 +1,11 @@
+using Avalonia.Media;
+
+namespace Klavier.UI.Theme;
+
+public static class KlavierTheme
+{
+    public static readonly Color AppBackground = Color.Parse("#1E1E1E");
+    public static readonly Color PanelBackground = Color.Parse("#2D2D2D");
+    public static readonly Color TextPrimary = Color.Parse("#E0E0E0");
+    public static readonly Color Accent = Color.Parse("#4A9EFF");
+}
